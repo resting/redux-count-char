1221597eb2bab3c65ef70d655279955eee215ba9
resting <roy.wong.80@gmail.com>
2017-06-05 15:48:14 +0800

    Now do the logic that returns the length of characters in text passed in. Remember to return a default case of state.

--- src/reducers/index.js
+++ src/reducers/index.js
@@ -1,7 +1,14 @@
 import { combineReducers } from 'redux';
 
 const countResult = (state={}, action) => {
-  return {};
+  switch (action.type) {
+    case 'COUNT_CHAR':
+      return {
+        totalChar: action.text.length
+      }
+    default:
+      return state;
+  }
 };
 
 export const appReducers = combineReducers({
